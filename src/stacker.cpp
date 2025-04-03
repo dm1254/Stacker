0af37796f1ecdccd90a48b2f55c71124ceadb0aa
dm1254 <d.moreno1272@gmail.com>
2025-04-03 16:46:06 -0700

    Fixed y axis incrementation functionality

--- src/stacker.cpp
+++ src/stacker.cpp
@@ -3,41 +3,46 @@
 #include <SPI.h>
 #include <Wire.h>
 
+
 U8G2_ST7920_128X64_1_SW_SPI u8g2(U8G2_R0, 13, 11, 10, 8);
 const int Button = 12;
 int block_Position_x[2] = {60,55};
 int block_Position_y = 124;
 int direction = 1;
-
-bool Debounce(int Button){
-  
-  static int buttonState = LOW;
-  static int lastButtonState = LOW; 
-  static unsigned long lastDebounceTime = 0;
-  static unsigned long debounceTime = 100;
-
-  int reading = digitalRead(Button);
-  if(reading != lastButtonState){
-    lastDebounceTime = millis();
-  }
-  if((millis() - lastDebounceTime)> debounceTime){
-    if(reading != buttonState){
-      buttonState = reading;
-      return true;
-    }
-  }
-  return false;
-}
+volatile bool buttonPressed = false;
+static unsigned long lastDebounceTime = 0;
+static unsigned long debounceDelay = 50;
+unsigned long previousMillis = 0;
+const long interval = 200;
+unsigned long lastButtonCheck = 0;
+const long buttonCheckInterval = 20;
+unsigned long lastPressTime = 0;
 
 
-void setup(void) {
+
+void setup() {
   Serial.begin(9600);
   u8g2.begin();
   pinMode(Button,INPUT);
+
 }
 
-void loop(void) {
-  bool buttonPressed = Debounce(Button);
+void checkButton() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastButtonCheck >= buttonCheckInterval) {
+    int buttonState = digitalRead(Button);
+    if (buttonState == HIGH && currentTime - lastPressTime > debounceDelay) {
+      buttonPressed = true;
+      lastPressTime = currentTime;
+    }
+    lastButtonCheck = currentTime;
+  }
+}
+
+
+void loop() {
+  unsigned long currentMillis = millis();
+  checkButton();
   u8g2.firstPage();
   do {
     
@@ -46,24 +51,25 @@ void loop(void) {
       u8g2.drawBox(block_Position_y,block_Position_x[i],4,4);
     }
   } while ( u8g2.nextPage() );
-  
 
-  if(buttonPressed){
-    block_Position_y -= 4;
-    u8g2.setCursor(50,30);
-    u8g2.print("pressed");
-  }else{
-    for(int i=0;i<2;i++){
+  if (buttonPressed){
+    buttonPressed = false;
+    delay(1000);
+    block_Position_y -=4;
+    block_Position_x[0] = 60;
+    block_Position_x[1] = 55;
+   
+  }
+  if(currentMillis - previousMillis >= interval){
+    previousMillis = currentMillis;
+    if (block_Position_x[0] >= 60) {
+      direction = -1;
+    } else if (block_Position_x[0] <= 9) {
+      direction = 1;
+    }
+    for (int i = 0; i < 2; i++) {
       block_Position_x[i] += 4 * direction;
     }
 
-    if(block_Position_x[0] >= 60){
-      direction = -1;
-    }else if(block_Position_x[0] <= 8){
-      direction = 1;
-    }
   }
-  delay(500);
 }
-
-
